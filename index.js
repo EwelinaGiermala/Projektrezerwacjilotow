9b2bcfcd2a628d8f3773dff94535bfbdee2d2471
Ewelina Giermala <ewelinagiermala@gmail.com>
2023-05-01 19:43:04 +0200

    Pojekt rezerwacji lotów poprawki

--- index.js
+++ index.js
@@ -22,48 +22,6 @@ seconds.textContent = sec;
 },1000);
 //  END DIGITAL CLOCK
 // START BOOKING TICKETS
-
-
-
-
-
-// const usernameSignUp=document.querySelector("#usernameSignUp").value;
-// const email=document.querySelector("#email").value;
-// const passSignUp=document.querySelector("#passSignUp").value;
-// const confPass=document.querySelector("#confpass").value;
-// const messageCheck1=document.querySelector("#messagecheck1");
-// const messageCheck2=document.querySelector("#messagecheck2");
-// const submitSignUp=document.querySelector("#submitSignUp");
-// submitSignUp.addEventListener("click", function(){
-// 	usernameSignUp=document.querySelector("#usernameSignUp").value;
-// 	email=document.querySelector("#email").value;
-// 	passSignUp=document.querySelector("#passSignUp").value;
-// 	confPass=document.querySelector("#confpass").value;
-// 	if(usernameSignUp==="" || email==="" || passSignUp==="" || confPass==="")
-// 	{
-// 		messageCheck1.innerHTML="All Fields Are Required"; 
-// 		if(passSignUp!=="" && confPass!=="" && passSignUp!==confPass)
-// 		{
-// 			messageCheck2.innerHTML="Password And Confirm Password Must Be Same";
-// 		}
-// 	}
-// 	else
-// 	{
-// 		messageCheck1.innerHTML=""; 
-// 		messagecheck2.innerHTML="";
-// 		if(passSignUp!==confPass)
-// 		{
-// 			messageCheck1.innerHTML="Password And Confirm Password Must Be Same";
-// 		}
-// 		else
-// 		{
-// 			messageCheck1.innerHTML=""; 
-// 			messagecheck2.innerHTML="";
-// 			alert("Thank You For Registering!!");
-// 		}
-// 	}
-// });
-
 const departure=document.querySelector("#inputGroupSelect01").value;
 const arrival=document.querySelector("#inputGroupSelect02").value;
 const dateDeparture=document.querySelector("#departure").value;
@@ -73,44 +31,21 @@ btnSearch.addEventListener("click",function(){
 	const departure=document.querySelector("#inputGroupSelect01").value;
 	const arrival=document.querySelector("#inputGroupSelect02").value;
 	const dateDeparture=document.querySelector("#departure").value;
-	const dateReturn=document.querySelector("#return").value;
+	const dateArrival=document.querySelector("#return").value;
 	const dD=parseInt(dateDeparture.substring(8,10));
-	const dR=parseInt(dateReturn.substring(8,10));
+	const dR=parseInt(dateArrival.substring(8,10));
 	if(departure==="departurePlace")
 	{
 		alert("Wybierz właściwe miejsce wylotu");
 	}
 	else if(arrival==="arrivalPlace")
 	{
-		alert("Wybierz właściwe miejsce docelowe");
-	}
-	else if(dateDeparture===""||dateReturn===""||dD>dR||dD<24||dR<24)
-	{
-		alert("Podaj właściwą datę");
+		alert("Wybierz właściwe miejsce przylotu");
+	} else {
+		window.location.href = "rezerwuj-loty.html?departure="+departure+"&arrival="+arrival+"&dateDeparture="+dateDeparture+"&dateArrival="+dateArrival+"&luggage="+luggage;
 	}
 });
 // END BOOKING TICKETS
- 
- 
- 
- 
- 
- 
- 
- 
-//  //DISABLED PAST DATES IN RESERVATION DATE
-// $(document).ready(function () {
-// var dateToday = new Date();
-// var month = dateToday.getMonth() + 1;
-// var day = dateToday.getDate();
-// var year = dateToday.getFullYear();
-// if (month < 10) month = "0" + month.toString();
-// if (day < 10) day = "0" + day.toString();
-// var maxDate = year + "-" + month + "-" + day;
-// $("#flight-date").attr("min", maxDate);
-// });
-
-
 
 
 
